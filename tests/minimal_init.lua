8caa1d10ef182e7ffa3373c0b93b508d4df5b834
Daniel Fakunle <tosinfakunle@yahoo.com>
2026-04-16 20:27:16 -0400

    feat: clean

--- tests/minimal_init.lua
+++
@@ -1,5 +0,0 @@
-local root = vim.fn.fnamemodify(debug.getinfo(1, 'S').source:sub(2), ':p:h:h')
-local plenary_path = root .. '/.deps/plenary.nvim'
-
-vim.opt.rtp:prepend(root)
-vim.opt.rtp:prepend(plenary_path)
